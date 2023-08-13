525c97ac126481398520b76a6d28c09200e64a44
jamili <jamili.amaral@estudante.ifms.edu.br>
2023-08-13 19:22:23 -0400

    arrumando o codigo

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const connectionDataBase = require('./config/connection');
 const express = require('express');
+const routes = require('./router/routes');
 const app = express();
 const port = 3003;
-app.use(express.json());
+app.use(express.json(), routes);
 app.listen(port, () => { console.log(`Run server...${port}`) });
+
 
 app.get('/', (req,res) => res.send("API-USER"));
 
-
-
+//app.post('/user', userController.createUser);
+//pp.get('/user', userController.findUser);
 
 //app.delete('/user', userController.deleteUser);
 
--- router/routes.js
+++ router/routes.js
@@ -1,17 +1,9 @@
-const connectionDataBase = require('./config/connection');
-const userController = require('./controller/userController')
 const express = require('express');
-const routes = require('./router/routes');
+const routes = express.Router();
 
-const app = express();
-const port = 3003;
-app.use(express.json(), routes);
-app.listen(port, () => { console.log(`Run server...${port}`) });
-
-app.get('/', (req,res) => res.send("API-USER"));
-
-  routes.get ('/user', userController.findUser);
-  routes.post ('/user', userController.createUser);
-  routes.post ('./user/authenticated', userController.authenticatedUser);
+const userController = require('../controller/userController');
+  routes.get('/user', userController.findUser);
+  routes.post('/user', userController.createUser);
+  routes.post('/user/authenticated', userController.authenticatedUser);
 
   module.exports = routes;
